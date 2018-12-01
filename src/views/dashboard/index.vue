f93432c2351c4af641623d0d8beb705b4e5edb31
Jean Barriere <jean@barriere.io>
2018-12-01 02:51:55 +0100

    feat(devbar): add dev bar

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,9 +2,21 @@
   <div>
     <div
       v-if="$broker.debug"
+      :class="{ show: bar }"
       class="broker-dev">
-      <span>Status {{ status }}</span>
-      <v-btn @click="$broker.setStatus(100)">Change Status</v-btn>
+      <span>Broker status: {{ status === 200 ? 'OK' : 'KO' }}</span>
+      <div class="actions">
+        <v-select
+          :items="notifs"
+          label="Notify"
+          @change="emitTestNotif" />
+        <v-btn @click="$broker.tryConnect(status === 200)">{{ status === 200 ? 'Disconnect' : 'Connect' }}</v-btn>
+      </div>
+      <div
+        class="switch"
+        @click="bar = !bar">
+        <i :class="`fa fa-${bar ? 'times' : 'plus'}`" />
+      </div>
     </div>
     <mc-js />
   </div>
@@ -17,7 +29,9 @@ export default {
   name: 'Dashboard',
   components: { McJs },
   data: () => ({
-    status: 0
+    status: 0,
+    bar: false,
+    notifs: ['error', 'info', 'warn', 'success']
   }),
   mounted: function () {
     console.log(this.$broker)
@@ -25,6 +39,11 @@ export default {
       this.status = status
     }
     this.status = this.$broker.status
+  },
+  methods: {
+    emitTestNotif: function (value) {
+      this.$notify({ title: 'sample notification', type: value })
+    }
   }
 }
 </script>
@@ -33,14 +52,41 @@ export default {
 .broker-dev {
   position: absolute;
   margin: 0;
-  padding: 0;
+  padding: 0 2rem;
   z-index: 99999;
-  top: 0;
+  bottom: -72px;
   left: 0;
   right: 0;
-  display: block;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-align: left;
   width: 100%;
   height: 72px;
   background-color: rgba(255, 255, 255, 0.25);
+  transition: all .2s ease-in;
+  &.show {
+    bottom: 0;
+  }
+  .switch {
+    background-color: rgba(255, 255, 255, .25);
+    position: absolute;
+    right: 20px;
+    top: -40px;
+    border-radius: .25rem .25rem 0 0;
+    height: 40px;
+    width: 40px;
+    display: flex;
+    padding: 0;
+    margin: 0;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .actions {
+    display: inline-flex;
+    align-items: center;
+    padding: 1rem;
+  }
 }
 </style>
