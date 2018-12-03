8d4dbd6835fe640d603626355b14a280afe5c8b1
Jean Barriere <jean@barriere.io>
2018-12-03 22:22:20 +0100

    chore(app): remove node-wifi dependency and disable the pi-wifi service for compilation purpose

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ import Broker from './Broker'
 import FacialRecognition from './FacialRecognition'
 import ProfileManager from './ProfileManager'
 import Watcher from './Watcher'
-import Wifi from './Wifi'
+// import Wifi from './Wifi'
 import VueMoment from 'vue-moment'
 
 Vue.use(Api)
@@ -12,6 +12,6 @@ Vue.use(Broker)
 Vue.use(FacialRecognition)
 Vue.use(ProfileManager)
 Vue.use(Watcher)
-Vue.use(Wifi)
+// Vue.use(Wifi)
 Vue.use(VueMoment)
 // Vue.use(HttpSpotifyService)
